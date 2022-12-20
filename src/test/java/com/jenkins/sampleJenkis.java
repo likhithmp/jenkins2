eff5ba6e14a3e1dda273a9aad5d54cf10252bea1
dell <likhith.mp94@gmail.com>
2022-12-20 22:46:13 +0530

    jenkins first commit

---
+++ src/test/java/com/jenkins/sampleJenkis.java
@@ -0,0 +1,14 @@
+package com.jenkins;
+
+import org.testng.annotations.Test;
+
+public class sampleJenkis {
+
+	
+	@Test(groups = "sanity")
+	public void test2() {
+		System.out.println("This is jenkins");
+		System.out.println("abc");
+	}
+
+}
