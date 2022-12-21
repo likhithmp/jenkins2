d2eb9b6aca3491a2bfdae1fc4289a36e6781fe96
likhithmp <likhith.mp94@gmail.com>
2022-12-21 15:03:27 +0530

    parameterized

---
+++ src/test/java/com/jenkins/parameterized.java
@@ -0,0 +1,14 @@
+package com.jenkins;
+
+import org.testng.annotations.Test;
+
+public class parameterized {
+
+	@Test
+	public void testParameter() {
+		System.out.println("excute parallel");
+		String BROWSER = System.getProperty("browser");
+		System.out.println("BROWSER==>"+ BROWSER);
+		
+	}
+}
