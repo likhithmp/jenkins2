8a3c7db3d2c1042f09b906e544f627542dfeb667
likhithmp <likhith.mp94@gmail.com>
2022-12-21 12:14:58 +0530

    pipe line jenkins

---
+++ src/test/java/com/jenkins/practiceSmokeJenkins.java
@@ -0,0 +1,13 @@
+package com.jenkins;
+
+import org.testng.annotations.Test;
+
+public class practiceSmokeJenkins {
+
+	@Test(groups = "smoke")
+	public void test2() {
+		System.out.println("This is smoke testing");
+		System.out.println("yuhj");
+		System.out.println("45678986");
+	}
+}
---
+++ src/test/java/com/jenkins/regression.java
@@ -0,0 +1,14 @@
+package com.jenkins;
+
+import org.testng.annotations.Test;
+
+public class regression {
+	
+	@Test(groups = "regression")
+	public void test2() {
+		System.out.println("This is regression testing");
+		System.out.println("xyz");
+		System.out.println("mp");
+	}
+
+}
